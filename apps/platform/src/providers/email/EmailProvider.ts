parcelvoy/platform

Add REST email provider
Added a REST email provider using nodemailer-rest-transport package.

apps/platform/src/providers/email/EmailProvider.ts
@@ -3,7 +3,7 @@ import { LoggerProviderName } from '../LoggerProvider'
 import Provider, { ProviderGroup } from '../Provider'
 import { Email } from './Email'
 
-export type EmailProviderName = 'ses' | 'smtp' | 'mailgun' | 'sendgrid' | LoggerProviderName
+export type EmailProviderName = 'ses' | 'smtp' | 'mailgun' | 'sendgrid' | 'rest' | LoggerProviderName
 
 export default abstract class EmailProvider extends Provider {
 

YouCruit/parcelvoy-platform

apps/platform/src/providers/email/EmailProvider.ts
@@ -3,7 +3,7 @@ import { LoggerProviderName } from '../LoggerProvider'
 import Provider, { ProviderGroup } from '../Provider'
 import { Email } from './Email'
 
-export type EmailProviderName = 'ses' | 'smtp' | 'mailgun' | 'sendgrid' | LoggerProviderName
+export type EmailProviderName = 'ses' | 'smtp' | 'mailgun' | 'sendgrid' | 'tachikoma' | LoggerProviderName
 
 export default abstract class EmailProvider extends Provider {
 